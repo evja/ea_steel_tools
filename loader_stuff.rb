165674a1bbdbe5f853e97d1e3f1c96179dd4f37b
Evan A <evjay.a@gmail.com>
2016-11-28 18:15:55 -0700

    deleted loader stuff file

--- loader_stuff.rb
+++
@@ -1,8 +0,0 @@
-#Initialize the console with some of these options to get working
-
-mod = Sketchup.active_model
-ents = Sketchup.active_model.entities
-sel = Sketchup.active_model.selection
-
-load 'ea_steel_tools/wide_flange_rolled_data.rb'
-load 'ea_steel_tools/dialog_rolled.rb'
