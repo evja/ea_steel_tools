492cef5c5638437352dd4e2cc24bbfb478a74e6e
Evan Allred <evjay.a@gmail.com>
2017-01-10 22:26:50 -0700

    updated the breakout

--- breakout.rb
+++ breakout.rb
@@ -4,163 +4,110 @@ module EA_Extensions623
   module EASteelTools
 		require 'sketchup.rb'
 		SKETCHUP_CONSOLE.show
-		GROUP_REGEX = /([A-Z,a-z]{2})(\d{3})/
-		BEAM_REGEX = /(([W,w])\d{1,2}([X,x])\d{1,3})/
-    BEAM_COLOR = 220,20,60
-    PLATE_COLOR = 'black'
 
     module BreakoutMod
-      def self.qualify_selection(sel)
-        if sel[0].class == Sketchup::Group && sel[0].name.match(GROUP_REGEX)
-          p 'passed as a group'
-          possibilities = []
-          sel[0].entities.each do |ent|
-            if ent.class == Sketchup::Group || ent.class == Sketchup::ComponentInstance
-              p sel[0].entities.count
-              if ent.name.match(BEAM_REGEX)
-                return true
-                possibilities.push 'True'
-              else
-                possibilities.push 'False'
-              end
-            else
-              next
-              p 'next'
-            end
-          end
-
-          p possibilities
-          # p possibilities.count
-          if possibilities.include?('True')
+      def self.qualify_model(model)
+        ents = model.entities
+        if model.entities.count == 1
+          if ents[0].class == Sketchup::Group && ents[0].name.match(GROUP_REGEX)
+            p 'passed as a group'
             return true
+
+          elsif ents[0].class == Sketchup::ComponentInstance && ents[0].definition.name.match(GROUP_REGEX)
+            p 'passed as a Component'
+            return true
+
           else
+            p 'not validated'
             return false
           end
-
-        elsif sel[0].class == Sketchup::ComponentInstance && sel[0].definition.name.match(GROUP_REGEX)
-          p 'passed as a Component'
-          possibilities = []
-            sel[0].definition.entities.each do |ent|
-              if ent.name.match(BEAM_REGEX)
-                return true
-                possibilities.push 'True'
-              else
-                possibilities.push 'False'
-              end
-
-              if possibilities.include?('True')
-                return true
-              else
-                return false
-              end
-            end
         else
-          p 'not validated'
           return false
         end
       end
     end
-
-
 
     class Breakout
 
       def initialize
         @model = Sketchup.active_model
         @entities = @model.entities
+        @materials = @model.materials
         @selection = @model.selection
         @plates = []
-        @steel_members = []
+        @steel_member = @entities.first
+        @member_name = @steel_member.name
         @labels = []
-	  		@multiple = ''
-	  		@path = @model.path
-	  		activise
+        activate
 			end
 
-  		def sanitize_selection(sel)
-  			if sel.count > 1
-          @multiple = true
-          p 'multiple'
-  				sel.each_with_index {|member, i| @steel_members.push validate_selection(member)}
-  			else
-          @multiple = false
-          p 'single'
-  				@steel_members.push validate_selection(sel[0])
-          @beam_name = @steel_members[0].name
-          p @steel_members
-  			end
-  		end
+      def activate
+        add_scenes
+        set_colors
+        position_member(@steel_member)
+        components = scrape(@steel_member)
+      end
 
-  		def validate_selection(sel)
-        p sel
-        p sel.class
-        p sel.name
-  			if sel.class == Sketchup::Group && sel.name.match(GROUP_REGEX)
-  				return sel
-        else
-          p'no joy in validate selection'
-  			end
-  		end
-
-  		def activise
-  			sanitize_selection(@selection)
-        @steel_members.each {|mem| scrape(mem)}
-        find_breakout_location
-        clear_model
-        create_new_file
-  		end
+      def set_colors
+        material_files = Sketchup.find_support_files('skm', 'Plugins/ea_steel_tools/Colors')
+        material_files.each do |m_file|
+          @materials.load(m_file)
+        end
+        @done_color = @materials['1 Done']
+        @steel_member.material = @done_color
+      end
 
       def is_plate?(entity)
         #write code to see if the part is a plate
       end
 
   		def scrape(part)
+        parts = []
         if part.class == Sketchup::Group
           p part.entities.count
     			part.entities.each do |e|
-            if e.class == Sketchup::Group && e.name.match(BEAM_REGEX)
-              e.material = BEAM_COLOR
+            if e.name.match(BEAM_REGEX)
+              # e.material = BEAM_COLOR
               # p e.name
               # p e.class
               # p e.definition
             end
 
             if e.class == Sketchup::Group && !e.name.match(BEAM_REGEX)
-              e.material = PLATE_COLOR
-              # p e.name
-              # p e.class
-              # p e.definition
+              if e.class == Sketchup::ComponentInstance
+                # e.material = PLATE_COLOR
+                # p e.class
+                # p e.definition
+              end
+
+              if e.class == Sketchup::Group
+                # e.material = PLATE_COLOR
+                # p e.class
+                # p e.definition
+              end
             end
 
-            if e.class == Sketchup::ComponentInstance
-              e.material = PLATE_COLOR
-              # p e.class
-              # p e.definition
-            end
-            # p e.name
-            # p e.class
-            # p e.bounds.height
-            # p e.bounds.width
-            # p e.bounds.depth
+
           end
+
         elsif part.class == Sketchup::ComponentInstance
           p part.definition.entities.count
           part.definition.entities.each do |e|
             if e.class == Sketchup::Group && e.name.match(BEAM_REGEX)
-              e.material = BEAM_COLOR
+              # e.material = BEAM_COLOR
               # p e.name
               # p e.class
               # p e.definition
             end
 
             if e.class == Sketchup::Group && !e.name.match(BEAM_REGEX)
-              e.material = BEAM_COLOR
+              # e.material = BEAM_COLOR
               # p e.name
               # p e.class
               # p e.definition
             end
             if e.class == Sketchup::ComponentInstance
-              e.material = PLATE_COLOR
+              # e.material = PLATE_COLOR
               # p e.class
               # p e.definition
             end
@@ -171,66 +118,30 @@ module EA_Extensions623
             # p e.bounds.depth
           end
         end
+        return parts
   		end
 
-      def check_for_duplicate_files(dir, file)
-        if File.exist?(file)
-          UI.messagebox('File already exists')
-          return false
-        else
-          return true
-        end
+      def add_scenes
+        pages = @model.pages
+        view = @model.active_view
+        perspective_scene = pages.add "Perspective"
+        front_scene = pages.add "Front"
+        plates_scene = pages.add "Plates"
+        pages.selected_page = pages[0]
       end
 
-      def clear_model
-        @entities.each do |ent|
-          if ent.class == Sketchup::Group || ent.class == Sketchup::ComponentInstance
-            if ent.hidden?
-              ent.hidden = false
-            end
-            if ent.locked?
-              ent.locked = false
-            end
-          end
-        end
-        @selection.add @entities.to_a
-        @selection.remove @steel_members.to_a
-        @entities.erase_entities(@selection.to_a)
+      def position_member(member)
+        tr = Geom::Transformation.axes ORIGIN, X_AXIS, Y_AXIS, Z_AXIS
+        member.move! tr
+        d = member.bounds.depth
+        h = member.bounds.height
+        w = member.bounds.width
+
+        x = X_AXIS.reverse
+        x.length = w/2
+        slide = Geom::Transformation.translation x
+        member.move! slide
       end
-
-      def create_new_file()
-        if @multiple
-          #dont do anything yet
-        else
-          @new_file = UI.savepanel("Save the Breakout", @path, "#{@beam_name}.skp" )
-          @model.save_copy(@new_file,Sketchup::Model::VERSION_2017)
-
-          # @sketchup = Sketchup.file_new
-          # @model_2 = @sketchup.active_model
-          # p1 = [0,0,0]
-          # p2 = [10,10,10]
-          # ents = @model_2.entities
-          # ents.add_line p1, p2
-          # @new_file = UI.savepanel("Save the Breakout", @path, "#{@beam_name}.skp" )
-          # Sketchup.open_file(File.join(@new_file))
-          UI.openURL(File.join(@new_file))
-        end
-      end
-
-      # def check_for_directory(dir)
-
-      # end
-
-      def find_breakout_location
-        f1 = 'Steel'
-        f2 = 'SketchUp Break-Outs'
-        d = @path.split('\\')
-        d.pop
-        d.pop
-        @path = File.join(d, f1, f2)
-        Dir.chdir("#{@path}")
-  		end
-
 
   	end
   end
