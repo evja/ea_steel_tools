9b1d946829164d6a30f860af2e5aa2da025750dc
Evan Allred <evjay.a@gmail.com>
2017-01-10 22:26:24 -0700

    Updated the code to send to breakout
    Sends to breakout and then has a "Breakout" context click
    has the ability to send to breakout multiple steel parts

--- steel_tools_menus.rb
+++ steel_tools_menus.rb
@@ -8,6 +8,8 @@ module EA_Extensions623
     require FNAME+'/'+'dialog_rolled.rb'
     require FNAME+'/'+'wide_flange_rolled_data.rb'
     require FNAME+'/'+'breakout.rb'
+    require FNAME+'/'+'breakout_send.rb'
+    require FNAME+'/'+'test.rb'
 
     # require 'hss_column_data.rb'         # Coming Soon
 
@@ -30,16 +32,16 @@ module EA_Extensions623
     toolbar = toolbar.add_item cmd
     toolbar.show
 
-    cmd = UI::Command.new("Rolled") {
+    cmd1 = UI::Command.new("Rolled") {
      Sketchup.active_model.select_tool @two = EASteelTools::RolledDialog.new
     }
-    @@EA_tools_menu.add_item cmd
-    cmd.small_icon = "icons/wfs_icon_rolled.png"
-    cmd.large_icon = "icons/wfs_icon_rolled.png"
-    cmd.tooltip = "Draw Rolled Wide Flange Steel"
-    cmd.status_bar_text = "Draw Rolled Steel"
-    cmd.menu_text = "Wide Rolled Flange Steel"
-    toolbar = toolbar.add_item cmd
+    @@EA_tools_menu.add_item cmd1
+    cmd1.small_icon = "icons/wfs_icon_rolled.png"
+    cmd1.large_icon = "icons/wfs_icon_rolled.png"
+    cmd1.tooltip = "Draw Rolled Wide Flange Steel"
+    cmd1.status_bar_text = "Draw Rolled Steel"
+    cmd1.menu_text = "Wide Rolled Flange Steel"
+    toolbar = toolbar.add_item cmd1
     toolbar.show
 
     # cmd = UI::Command.new("HSS") {
@@ -55,7 +57,17 @@ module EA_Extensions623
     # toolbar.show
 
     UI.add_context_menu_handler do |menu|
-      if( EASteelTools::BreakoutMod.qualify_selection(Sketchup.active_model.selection) )
+      if not( EASteelTools::BreakoutMod.qualify_model(Sketchup.active_model) )
+        if( EASteelTools::BreakoutSendMod.qualify_selection(Sketchup.active_model.selection) )
+          menu.add_separator
+          menu.add_item("Send to Breakout") { EASteelTools::SendToBreakout.new }
+          menu.add_separator
+        end
+      end
+    end
+
+    UI.add_context_menu_handler do |menu|
+      if( EASteelTools::BreakoutMod.qualify_model(Sketchup.active_model) )
         menu.add_separator
         menu.add_item("Breakout") { EASteelTools::Breakout.new }
         menu.add_separator
